67dd1a8595c49679168ecdf25fd383b93b678e18
Joshua <joshua.blackmore@comparethemarket.com>
2025-07-08 17:05:58 +0100

    removed bootstap installed tailwind and daisyui created navitems

--- Models/NavItem.cs
+++ Models/NavItem.cs
@@ -3,6 +3,12 @@ namespace portfolio_dotnet.Models;
 public class NavItem
 {
     public required string Text { get; set; }
-    public required string Controller { get; set; }
+    public string? Controller { get; set; }
     public string Action { get; set; } = "Index";
+
+    // External links (GitHub, LinkedIn, etc.)
+    public string? Href { get; set; }
+
+    // Optional: icon name or CSS class
+    public string? Icon { get; set; }
 }
